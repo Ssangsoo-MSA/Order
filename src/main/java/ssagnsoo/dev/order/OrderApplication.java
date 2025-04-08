930d78a2f574559853c21c287d67ceddbdff0396
SsangSoo <kss10080@naver.com>
2025-04-08 18:01:27 +0900

    project init

---
+++ src/main/java/ssagnsoo/dev/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package ssagnsoo.dev.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ssagnsoo/dev/order/entity/Order.java
@@ -0,0 +1,19 @@
+package ssagnsoo.dev.order.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "orders")
+@Getter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private
+}
